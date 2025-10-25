b78e694d19f2b6925a8a0c7db8f209a64f5d62c7
Michael <michaelpetrosyan4@gmail.com>
2025-10-25 19:36:00 +0400

    added for rosalind

--- tests/validate_DNA.py
+++ tests/validate_DNA.py
@@ -3,6 +3,7 @@ from DNA.DNA_helpers.helpers_DNA import DNA_helpers, read_code_from_file
 
 code = read_code_from_file('../src/DNA/gene.fna')
 test_DNA = DNA_helpers(code)
-print(test_DNA.composition())
-print(test_DNA.validate_DNA())
-
+# print(test_DNA.composition())
+# print(test_DNA.validate_DNA())
+# print(test_DNA.DNAstrand())
+print(code)
